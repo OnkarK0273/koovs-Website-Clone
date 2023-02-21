2f7a0866a6c4dc4ead1d8a67eed19d37a0a1aa15
Suman Biswas <sumanmanner@gmail.com>
2023-02-21 23:16:30 +0530

    updated today day-1

---
+++ koovs.com/src/Pages/Cart/Temp.tsx
@@ -0,0 +1,5 @@
+const Temp = () => {
+  return;
+};
+
+export default Temp;
--- koovs.com/src/utils/types.ts
+++ koovs.com/src/utils/types.ts
@@ -10,6 +10,16 @@ export interface Product {
   is_best_seller: boolean;
   Order_status: string;
   isOrdered: boolean;
+  size?: string;
 }
 
-
+export type CartItemType = {
+  id: number;
+  title: string;
+  price: number;
+  img1: string;
+  category: string;
+  size: string;
+  color: string;
+  quantity: number;
+};
