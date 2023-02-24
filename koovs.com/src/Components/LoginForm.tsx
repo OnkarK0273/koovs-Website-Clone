78265e758ea4183d53d4301bc26256526098ffe3
arun24hrs <2017arun.rana@gmail.com>
2023-02-25 04:14:56 +0530

    added login signup functions

--- koovs.com/src/Components/LoginForm.tsx
+++ koovs.com/src/Components/LoginForm.tsx
@@ -3,10 +3,78 @@ import { Image } from "@chakra-ui/image";
 import { Input } from "@chakra-ui/input";
 import { Box, Divider, Flex, Heading, Link, Text, VStack } from "@chakra-ui/layout";
 import React from "react";
-import fb from "../Utils/fbIcon.png";
-import google from "../Utils/googleIcon.png";
+import fb from "../Utils/Images/fbIcon.png";
+import google from "../Utils/Images/googleIcon.png";
+import { LoginDetail } from "../utils/types";
+
+
+interface StoreLoginDetail  {
+  login: false;
+  details: LoginDetail
+}
 
 const LoginForm = (): JSX.Element => {
+
+  const [loginDetails, setLoginDetails] = React.useState<StoreLoginDetail>({
+    login: false,
+    details: {email:"",password:""}
+  });
+  const [passIsEmpty, setPassIsEmpty] = React.useState<boolean>(false);
+  const [emailIsEmpty, setEmailIsEmpty] = React.useState<boolean>(false);
+
+
+  // login functions
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>):void => {
+    const newDetails = {
+      ...loginDetails,
+      details: { ...loginDetails.details, [e.target.name]: e.target.value },
+    };
+    setLoginDetails(newDetails);
+    setPassIsEmpty(false);
+    setEmailIsEmpty(false);
+  };
+
+  const handleFormSubmit = () => {
+    if (loginDetails == null) {
+      window.alert("Please fill the form!");
+      return;
+    } else if (
+      loginDetails.details.email == undefined ||
+      loginDetails.details.email == ""
+    ) {
+      setEmailIsEmpty(true);
+      return;
+    } else if (
+      loginDetails.details.password == undefined ||
+      loginDetails.details.password == ""
+    ) {
+      setPassIsEmpty(true);
+      return;
+    }
+    setPassIsEmpty(false);
+    setEmailIsEmpty(false);
+
+    // signupStore.details.map((el:any) => {
+    //   if (
+    //     el.email == loginDetails.details.email ||
+    //     el.mobile == loginDetails.details.email
+    //   ) {
+    //     if (el.password == loginDetails.details.password) {
+    //       dispatch(loginSuccess(el));
+    //       onLoginClose();
+    //       toast({
+    //         title: "Login Successfull",
+    //         status: "success",
+    //         duration: 9000,
+    //         isClosable: true,
+    //       });
+    //       location.reload();
+    //       return;
+    //     }
+    //   }
+    // });
+  };
   return (
     <VStack
     
@@ -16,10 +84,11 @@ const LoginForm = (): JSX.Element => {
       padding={"10px"}
     >
         <Heading size={"md"}>Log In</Heading>
-      <Input type="email" placeholder="Email" />
-      <Input type="password" placeholder="Password" />
+      <Input type="email" placeholder="Email" name="email" onChange={(e)=>handleChange(e)}/>
+      <Input type="password" placeholder="Password" name="password" onChange={(e)=>handleChange(e)}/>
       <Text pt={"15px"} textDecoration={"underline"}>Forgot Your Password?</Text>
       <Button
+      onClick={handleFormSubmit}
         variant={"solid"}
         bgColor={"black"}
         color={"white"}
--- koovs.com/src/Components/SignupForm.tsx
+++ koovs.com/src/Components/SignupForm.tsx
@@ -5,8 +5,72 @@ import { Box, Divider, Flex, Heading, Link, Text, VStack } from "@chakra-ui/layo
 import React from "react";
 import fb from "../Utils/fbIcon.png";
 import google from "../Utils/googleIcon.png";
+import {SignupDetail} from "../utils/types"
+import { useDispatch } from "react-redux";
+
 
 const SignupForm = (): JSX.Element => {
+  const dispatch = useDispatch();
+  const [signupDetails, setSignupDetails] = React.useState<SignupDetail>({
+    fName:"",
+    lName:"",
+    email:"",
+    password:""
+  })
+  const [insecurePassword, setInsecurePassword] = React.useState<boolean>(false);
+  const [invalidEmail, setInvalidEmail] = React.useState<boolean>(false);
+
+  const handleDetails = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const newDetails:SignupDetail = {
+      ...signupDetails,
+      [e.target.name]: e.target.value,
+    };
+
+    setSignupDetails(newDetails);
+    setInvalidEmail(false);
+    setInsecurePassword(false);
+  }
+
+  const handleSignupFormSubmit = () => {
+    let isValid = true;
+    Object.keys(signupDetails).forEach((key:string) => {
+      const value:string = signupDetails[key];
+
+      if (!value) {
+        isValid = false;
+      }
+    });
+
+    if (!isValid) {
+      window.alert("Please fill the form!");
+      return;
+    } else if (
+      signupDetails.email == "" ||
+      (signupDetails.email &&
+        (!signupDetails.email.includes("@")))
+    ) {
+      setInvalidEmail(true);
+      return;
+    } else if (
+      signupDetails.password.length >= 8 &&
+      signupDetails.password.match(/[!\@\#\$\%\^\&\*\+\-]/)
+    ) {
+    } else if (
+      signupDetails.password.length < 8 &&
+      signupDetails.password.match(/[!\@\#\$\%\^\&\*\+\-]/)
+    ) {
+      setInsecurePassword(true);
+      return;
+    } else if (!signupDetails.password.match(/[!\@\#\$\%\^\&\*\+\-]/)) {
+      setInsecurePassword(true);
+      return;
+    }
+
+    setInvalidEmail(false);
+    setInsecurePassword(false);
+    // dispatch(signupSuccess(signupDetails));
+  };
+
   return (
     <VStack
     
@@ -15,13 +79,14 @@ const SignupForm = (): JSX.Element => {
       alignItems={"left"}
       padding={"10px"}
     >
-        <Heading size={"md"} fontWeight={"semibold"}>Register</Heading>
-      <Input type="text" placeholder="First Name" />
-      <Input type="text" placeholder="Last Name" />
-      <Input type="email" placeholder="Email" />
-      <Input type="password" placeholder="Password" />
+      <Heading size={"md"} fontWeight={"semibold"} >Register</Heading>
+      <Input type="text" placeholder="First Name" name="fname" onChange={(e)=> handleDetails(e)}/>
+      <Input type="text" placeholder="Last Name" name="lname" onChange={(e)=> handleDetails(e)}/>
+      <Input type="email" placeholder="Email" name="email" onChange={(e)=> handleDetails(e)}/>
+      <Input type="password" placeholder="Password" name="password" onChange={(e)=> handleDetails(e)}/>
       <Text pt={"15px"} fontWeight={"normal"}>Sign up for early Sale access plus tailored new arrivals, trends and promotions. To opt out, click unsubscribe in our emails.</Text>
       <Button
+        onClick={handleSignupFormSubmit}
         variant={"solid"}
         bgColor={"black"}
         color={"white"}
