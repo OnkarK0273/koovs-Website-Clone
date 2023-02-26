08b9f818fc6592ebc1deaa53b6c808c6a5906dde
DeepakPatil26 <deepakpatil2612@gmail.com>
2023-02-26 19:30:46 +0530

    update files

--- koovs.com/src/Pages/WomenProductCard.tsx
+++ koovs.com/src/Pages/WomenProductCard.tsx
@@ -11,7 +11,6 @@ import {
 } from "@chakra-ui/react";
 import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import Footer from "../HomeComponent/Footer";
 import { Product } from "../utils/types";
 
 const data = {
@@ -23,39 +22,6 @@ const data = {
   rating: 4.2,
   numReviews: 34,
 };
-
-// interface RatingProps {
-//   rating: number;
-//   numReviews: number;
-// }
-
-// function Rating({ rating, numReviews }: RatingProps) {
-//   return (
-//     <Box display="flex" alignItems="center">
-//       {Array(5)
-//         .fill("")
-//         .map((_, i) => {
-//           const roundedRating = Math.round(rating * 2) / 2;
-//           if (roundedRating - i >= 1) {
-//             return (
-//               <BsStarFill
-//                 key={i}
-//                 style={{ marginLeft: "1" }}
-//                 color={i < rating ? "teal.500" : "gray.300"}
-//               />
-//             );
-//           }
-//           if (roundedRating - i === 0.5) {
-//             return <BsStarHalf key={i} style={{ marginLeft: "1" }} />;
-//           }
-//           return <BsStar key={i} style={{ marginLeft: "1" }} />;
-//         })}
-//       <Box as="span" ml="2" color="gray.600" fontSize="sm">
-//         {numReviews} review{numReviews > 1 && "s"}
-//       </Box>
-//     </Box>
-//   );
-// }
 
 function WomenProductCard({
   id,
@@ -92,7 +58,7 @@ function WomenProductCard({
               bg="red.200"
             />
           )}
-          <Link to={`/men/:${id}`}>
+          <Link to={`/women/${id}`}>
             <Image src={img1} alt={`Picture of ${title}`} roundedTop="lg" />
           </Link>
 
@@ -111,6 +77,7 @@ function WomenProductCard({
                 as="h4"
                 lineHeight="tight"
                 isTruncated
+                color={"black"}
               >
                 {title}
               </Box>
@@ -141,9 +108,6 @@ function WomenProductCard({
           </Box>
         </Box>
       </Flex>
-      <div>
-        <Footer />
-      </div>
     </>
   );
 }
