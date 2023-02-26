385ba755b000ddd3a1224481dd4e664696e70581
arun24hrs <2017arun.rana@gmail.com>
2023-02-26 18:41:00 +0530

    added back to top button

--- koovs.com/src/HomeComponent/BackToTop.tsx
+++ koovs.com/src/HomeComponent/BackToTop.tsx
@@ -1,12 +1,28 @@
-import { ArrowUpIcon } from '@chakra-ui/icons'
-import { Button, Link } from '@chakra-ui/react'
-
+import { JSXElement } from "@babel/types";
+import { ArrowUpIcon } from "@chakra-ui/icons";
+import { Button, Link } from "@chakra-ui/react";
 
 const BackToTop = () => {
   return (
-    <Link href="#home"><Button borderRadius="50%" w="30px" h="40px" bgColor={"black"} color="white" _hover={{bgColor:"gray.800", transform: "scale(1.2)"}} _active={{bgColor:"white", color: "black"}} position={"fixed"}top={"88%"} left={"95%"} zIndex={3} ><ArrowUpIcon fontSize={"30px"}/></Button></Link>
-    )
+    <>
+    <Link href="#home">
+      <Button
+        borderRadius="50%"
+        w="30px"
+        h="40px"
+        bgColor={"black"}
+        color="white"
+        _hover={{ bgColor: "gray.800", transform: "scale(1.2)" }}
+        _active={{ bgColor: "white", color: "black" }}
+        position={"fixed"}
+        top={"88%"}
+        left={"95%"}
+        zIndex={3}
+      >
+        <ArrowUpIcon fontSize={"30px"} />
+      </Button>
+    </Link></>
+  );
+};
 
-}
-
-export default BackToTop
+export default BackToTop;
